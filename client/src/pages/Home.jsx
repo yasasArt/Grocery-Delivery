46f73a624bce033bcd83362342a2d78dff5f3e7d
yasasArt <yasasjayaweera945@gmail.com>
2025-06-15 21:39:48 +0530

    Add banner
    banner

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MainBanner from '../components/MainBanner'
+
+const Home = () => {
+  return (
+    <div className='mt-10'>
+        <MainBanner />
+
+    </div>
+  )
+}
+
+export default Home
