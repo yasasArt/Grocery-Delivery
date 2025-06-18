fa849e9401250d4ad7ab63886ced1982fce660a8
yasasArt <yasasjayaweera945@gmail.com>
2025-06-18 20:29:39 +0530

    bottom banner

---
+++ client/src/components/BottomBanner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const BottomBanner = () => {
+  return (
+    <div className='relative mt-16'>
+        <img src="bottomBanner.jpg" alt="Bottom Banner" className='w-full  object-cover' />
+        <img src="bottomBanner.jpg" alt="Bottom Banner" className='w-full md:hidden' />
+
+    </div>
+  )
+}
+
+export default BottomBanner
