474909c0cc69c308e01f1644ffa35dc10a628879
yasasArt <yasasjayaweera945@gmail.com>
2025-06-18 21:58:02 +0530

    footer

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,46 @@
+const Footer = () => {
+    const linkSections = [
+        {
+            title: "Quick Links",
+            links: ["Home", "Best Sellers", "Offers & Deals", "Contact Us", "FAQs"]
+        },
+        {
+            title: "Need Help?",
+            links: ["Delivery Information", "Return & Refund Policy", "Payment Methods", "Track your Order", "Contact Us"]
+        },
+        {
+            title: "Follow Us",
+            links: ["Instagram", "Twitter", "Facebook", "YouTube"]
+        }
+    ];
+
+    return (
+        <div className="px-6 md:px-16 lg:px-24 xl:px-32 bg-primary/10">
+            <div className="flex flex-col md:flex-row items-start justify-between gap-10 py-10 border-b border-gray-500/30 text-gray-500">
+                <div>
+                    <img className="w-34 md:w-32" src="profile.png" alt="dummyLogoColored" />
+                    <p className="max-w-[410px] mt-6">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum unde quaerat eveniet cumque accusamus atque qui error quo enim fugiat?</p>
+                </div>
+                <div className="flex flex-wrap justify-between w-full md:w-[45%] gap-5">
+                    {linkSections.map((section, index) => (
+                        <div key={index}>
+                            <h3 className="font-semibold text-base text-gray-900 md:mb-5 mb-2">{section.title}</h3>
+                            <ul className="text-sm space-y-1">
+                                {section.links.map((link, i) => (
+                                    <li key={i}>
+                                        <a href="#" className="hover:underline transition">{link}</a>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <p className="py-4 text-center text-sm md:text-base">
+                NatureCart Grocery Store © 2025. All rights reserved.
+            </p>
+        </div>
+    );
+};
+
+export default Footer;
