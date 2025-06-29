8ec8cba40a156c81655c5eb2a088c3288a1ee79f
yasasArt <yasasjayaweera945@gmail.com>
2025-06-29 23:55:24 +0530

    oderRoute change

--- server/routes/oderRoute.js
+++ server/routes/oderRoute.js
@@ -1,12 +1,11 @@
 import express from 'express';
-import authSeller from '../middlewares/authSeller';
-import { getAllOrders,getUserOrders, placeOrderCOD } from '../controllers/oderController';
 import authSeller from '../middlewares/authSeller.js';
+import { getAllOrders, getUserOrders, placeOrderCOD } from '../controllers/oderController.js';
 
 const oderRouter = express.Router();
 
-oderRouter.post('/cod', authSeller,placeOrderCOD);
-oderRouter.get('/user', authSeller,getUserOrders);
-oderRouter.get('/seller', authSeller,getAllOrders);
+oderRouter.post('/cod', authSeller, placeOrderCOD);
+oderRouter.get('/user', authSeller, getUserOrders);
+oderRouter.get('/seller', authSeller, getAllOrders);
 
 export default oderRouter;
